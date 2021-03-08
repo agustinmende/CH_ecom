58b934ee232b8e2fb0d3ea82b25ca25590743125
Agustin <agustin.mende@thepasto.com>
2021-03-07 23:56:05 -0300

    Agregado NavBar - entrega #2

---
+++ mate/src/components/NavBar.js
@@ -0,0 +1,23 @@
+
+function NavBar() {
+  return (
+
+    <div className="NavBar">
+      <h1>Mate</h1>
+      <ul>
+          <li><a href="#">Inicio</a></li>
+      </ul>
+      <ul>
+          <li><a href="#">Productos</a></li>
+      </ul>
+      <ul>
+          <li><a href="#">Metodos de pago</a></li>
+      </ul>
+      <ul>
+          <li><a href="#">Contacto</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
