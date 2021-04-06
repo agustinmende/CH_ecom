ad89a7ca5b9e906380e463f1ad957bb802c162ec
Agustin Mende <agusm@bitgeyser.com>
2021-04-06 11:39:25 -0300

    Update Item.js

--- mate/src/components/Item.js
+++ mate/src/components/Item.js
@@ -9,9 +9,9 @@ function Item({id, item }) {
             <p className="price">$ {item.price}</p>
 
             <div className="ring-overlay">
-                <a className="buynow" href="">Buy Now</a>
+               {/* <a className="buynow" href="">Buy Now</a>
                 <a className="save" href="">Save</a>
-                <a className="addtocart" href="">Add to cart</a>
+                <a className="addtocart" href="">Add to cart</a> */}
                 <p className="description">{item.description}</p>
                 <p><Link to={`/item/${id}`}>Ver Más...</Link></p>
             </div>
